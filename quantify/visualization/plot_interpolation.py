fcf4f5bc054cff80b28b3c4ede91e11c23d7b92e
Adriaan Rol <adriaan.rol@gmail.com>
2020-05-04 22:26:19 +0200

    live plotting for 2D interpolated experiments

---
+++ quantify/visualization/plot_interpolation.py
@@ -0,0 +1,112 @@
+import numpy as np
+import logging
+from scipy import interpolate
+
+
+def areas(ip):
+    p = ip.tri.points[ip.tri.vertices]
+    q = p[:, :-1, :] - p[:, -1, None, :]
+    areas = abs(q[:, 0, 0] * q[:, 1, 1] - q[:, 0, 1] * q[:, 1, 0]) / 2
+    return areas
+
+
+def scale(points, xy_mean, xy_scale):
+    points = np.asarray(points, dtype=float)
+    return (points - xy_mean) / xy_scale
+
+
+def unscale(points, xy_mean, xy_scale):
+    points = np.asarray(points, dtype=float)
+    return points * xy_scale + xy_mean
+
+
+def interpolate_heatmap(x, y, z, n: int = None, interp_method: str = "linear"):
+    """
+    Args:
+        x   (array): x data points
+        y   (array): y data points
+        z   (array): z data points
+        n     (int): number of points for each dimension on the interpolated
+            grid if set to None will auto determine amount of points needed
+        interp_method (str): on of {"linear", "nearest", "deg"}, determines
+            what interpolation method is used.
+
+    Returns:
+        x_grid : N*1 array of x-values of the interpolated grid
+        y_grid : N*1 array of x-values of the interpolated grid
+        z_grid : N*N array of z-values that form a grid.
+
+
+    The output of this method can directly be used for
+        plt.imshow(z_grid, extent=extent, aspect='auto')
+        where the extent is determined by the min and max of the x_grid and
+        y_grid.
+
+    The output can also be used as input for
+        ax.pcolormesh(x, y, Z,**kw)
+
+    """
+
+    points = list(zip(x, y))
+    lbrt = np.min(points, axis=0), np.max(points, axis=0)
+    lbrt = lbrt[0][0], lbrt[0][1], lbrt[1][0], lbrt[1][1]
+
+    xy_mean = np.mean([lbrt[0], lbrt[2]]), np.mean([lbrt[1], lbrt[3]])
+    xy_scale = np.ptp([lbrt[0], lbrt[2]]), np.ptp([lbrt[1], lbrt[3]])
+
+    # interpolation needs to happen on a rescaled grid, this is somewhat akin
+    # to an assumption in the interpolation that the scale of the experiment
+    # is chosen sensibly. N.B. even if interp_method == "nearest" the linear
+    # interpolation is used to determine the amount of grid points.
+    ip = interpolate.LinearNDInterpolator(
+        scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z
+    )
+
+    if n is None:
+        # Calculate how many grid points are needed.
+        # factor from A=√3/4 * a² (equilateral triangle)
+        # N.B. a factor 4 was added as there were to few points for uniform
+        # grid otherwise.
+        all_areas = areas(ip)
+        area_min = all_areas[all_areas > 0.].min()
+        n = int(0.658 / np.sqrt(area_min)) * 4
+        n = max(n, 10)
+        if n > 500:
+            logging.debug("n: {} larger than 500".format(n))
+            n = 500
+
+    x_lin = y_lin = np.linspace(-0.5, 0.5, n)
+
+    if interp_method == "linear":
+        z_grid = ip(x_lin[:, None], y_lin[None, :]).squeeze()
+    elif interp_method == "nearest":
+        ip = interpolate.NearestNDInterpolator(
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z
+        )
+        z_grid = ip(x_lin[:, None], y_lin[None, :]).squeeze()
+    elif interp_method == "deg":
+        # Circular interpolation in deg units
+        phases = np.deg2rad(z)
+        newdata_cos = np.cos(phases)
+        newdata_sin = np.sin(phases)
+
+        ip_cos = interpolate.LinearNDInterpolator(
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_cos
+        )
+        newdata_cos = ip_cos(x_lin[:, None], y_lin[None, :]).squeeze()
+
+        ip_sin = interpolate.LinearNDInterpolator(
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_sin
+        )
+        newdata_sin = ip_sin(x_lin[:, None], y_lin[None, :]).squeeze()
+
+        z_grid = (np.rad2deg(np.arctan2(newdata_sin, newdata_cos)) %
+                  360).squeeze()
+
+    # x and y grid points need to be rescaled from the linearly chosen points
+    points_grid = unscale(list(zip(x_lin, y_lin)),
+                          xy_mean=xy_mean, xy_scale=xy_scale)
+    x_grid = points_grid[:, 0]
+    y_grid = points_grid[:, 1]
+
+    return x_grid, y_grid, (z_grid).T
--- quantify/visualization/pyqt_plotmon.py
+++ quantify/visualization/pyqt_plotmon.py
@@ -3,6 +3,8 @@ Module containing the pyqtgraph based plotting monitor.
 """
 import numpy as np
 
+from .plot_interpolation import interpolate_heatmap
+
 from qcodes.instrument.parameter import ManualParameter
 from qcodes import validators as vals
 from qcodes.plots.colors import color_cycle
@@ -54,6 +56,9 @@ class PlotMonitor_pyqt(Instrument):
 
         # convenient access to curve variable for updating
         self._curves = []
+        self._im_curves = []
+        self._im_scatters = []
+        self._im_scatters_last = []
 
     def create_plot_monitor(self):
         """
@@ -85,6 +90,9 @@ class PlotMonitor_pyqt(Instrument):
         if self.secondary_QtPlot.traces != []:
             self.secondary_QtPlot.clear()
         self.curves = []
+        self._im_curves = []
+        self._im_scatters = []
+        self._im_scatters_last = []
 
         # TODO add persistence based on previous dataset
         dset = load_dataset(tuid=tuid)
@@ -92,6 +100,7 @@ class PlotMonitor_pyqt(Instrument):
         set_parnames = list(filter(lambda k: 'x' in k, dset.keys()))
         get_parnames = list(filter(lambda k: 'y' in k, dset.keys()))
 
+        #############################################################
         plot_idx = 1
         for yi in get_parnames:
             for xi in set_parnames:
@@ -111,6 +120,7 @@ class PlotMonitor_pyqt(Instrument):
                 self.curves.append(self.main_QtPlot.traces[-1])
             self.main_QtPlot.win.nextRow()
 
+        #############################################################
         # Add a square heatmap
         if dset.attrs['2D-grid']:
             plot_idx = 1
@@ -141,6 +151,60 @@ class PlotMonitor_pyqt(Instrument):
                 self.secondary_QtPlot.add(**config_dict)
                 plot_idx += 1
 
+        #############################################################
+        # if data is not on a grid but is 2D it makes sense to interpolate
+
+        elif len(set_parnames) == 2:
+            plot_idx = 1
+            for yi in get_parnames:
+
+                cmap = 'viridis'
+                zrange = None
+
+                config_dict = {
+                    "x": [0, 1],
+                    "y": [0, 1],
+                    "z": np.zeros([2, 2]),
+                    "xlabel": dset['x0'].attrs['long_name'],
+                    "xunit": dset['x0'].attrs['unit'],
+                    "ylabel": dset['x1'].attrs['long_name'],
+                    "yunit": dset['x1'].attrs['unit'],
+                    "zlabel": dset[yi].attrs['long_name'],
+                    "zunit": dset[yi].attrs['unit'],
+                    "subplot": plot_idx,
+                    "cmap": cmap,
+                }
+                if zrange is not None:
+                    config_dict["zrange"] = zrange
+                self.secondary_QtPlot.add(**config_dict)
+                self._im_curves.append(self.secondary_QtPlot.traces[-1])
+
+                # used to mark the interpolation points
+                self.secondary_QtPlot.add(
+                    x=[], y=[],
+                    pen=None,
+                    color=1.0, width=0, symbol="o", symbolSize=4,
+                    subplot=plot_idx,
+                    xlabel=dset['x0'].attrs['long_name'],
+                    xunit=dset['x0'].attrs['unit'],
+                    ylabel=dset['x1'].attrs['long_name'],
+                    yunit=dset['x1'].attrs['unit'],)
+                self._im_scatters.append(self.secondary_QtPlot.traces[-1])
+
+                # used to mark the last N-interpolation points
+                self.secondary_QtPlot.add(
+                    x=[], y=[],
+                    color=color_cycle[3],  # marks the point red
+                    width=0, symbol="o", symbolSize=7,
+                    subplot=plot_idx,
+                    xlabel=dset['x0'].attrs['long_name'],
+                    xunit=dset['x0'].attrs['unit'],
+                    ylabel=dset['x1'].attrs['long_name'],
+                    yunit=dset['x1'].attrs['unit'],)
+                self._im_scatters_last.append(self.secondary_QtPlot.traces[-1])
+
+                plot_idx += 1
+
     def update(self):
 
         if self.tuid() == 'latest':
@@ -161,6 +225,8 @@ class PlotMonitor_pyqt(Instrument):
             set_parnames = list(filter(lambda k: 'x' in k, dset.keys()))
             get_parnames = list(filter(lambda k: 'y' in k, dset.keys()))
             # Only updates the main monitor currently.
+
+            #############################################################
             i = 0
             for yi in get_parnames:
                 for xi in set_parnames:
@@ -169,6 +235,7 @@ class PlotMonitor_pyqt(Instrument):
                     i += 1
             self.main_QtPlot.update_plot()
 
+            #############################################################
             # Add a square heatmap
             if dset.attrs['2D-grid']:
                 for yidx, yi in enumerate(get_parnames):
@@ -176,4 +243,41 @@ class PlotMonitor_pyqt(Instrument):
                                    (dset.attrs['xlen'], dset.attrs['ylen']),
                                    order='F').T
                     self.secondary_QtPlot.traces[yidx]['config']['z'] = Z
+                self.secondary_QtPlot.update_plot()
+
+            #############################################################
+            # if data is not on a grid but is 2D it makes sense to interpolate
+            elif len(set_parnames) == 2:
+                for yidx, yi in enumerate(get_parnames):
+                    # exists to force reset the x- and y-axis scale
+                    new_sc = TransformState(0, 1, True)
+
+                    x = dset['x0'].values[~np.isnan(dset)['y0']]
+                    y = dset['x1'].values[~np.isnan(dset)['y0']]
+                    z = dset[yi].values[~np.isnan(dset)['y0']]
+                    # interpolation needs to be meaningful
+                    if len(z) < 8:
+                        break
+                    x_grid, y_grid, z_grid = interpolate_heatmap(
+                        x=x, y=y, z=z,
+                        interp_method='linear')
+
+                    trace = self._im_curves[yidx]
+                    trace['config']['x'] = x_grid
+                    trace['config']['y'] = y_grid
+                    trace['config']['z'] = z_grid
+                    # force rescale axis so marking datapoints works
+                    trace["plot_object"]["scales"]["x"] = new_sc
+                    trace["plot_object"]["scales"]["y"] = new_sc
+
+                    # Mark all measured points on which the interpolation
+                    # is based
+                    trace = self._im_scatters[yidx]
+                    trace['config']['x'] = x
+                    trace['config']['y'] = y
+
+                    trace = self._im_scatters_last[yidx]
+                    trace['config']['x'] = x[-5:]
+                    trace['config']['y'] = y[-5:]
+
                 self.secondary_QtPlot.update_plot()
