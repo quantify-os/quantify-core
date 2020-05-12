0b2126210f3bfea035a6edbaa01287e67e2803a5
Adriaan Rol <adriaan.rol@gmail.com>
2020-05-12 13:12:51 +0200

    created a data module

---
+++ quantify/data/data.py
@@ -0,0 +1,37 @@
+import datetime
+
+
+class TUID(str):
+    """
+    A human readable unique identifier based on the timestamp.
+
+    A tuid is a string formatted as YYYYMMDD-HHMMSS-fff-******.
+    The tuid serves as a unique identifier for experiments in quantify see
+    also :mod:`quantify.data`.
+    """
+
+    def __init__(self, value):
+        self.is_valid(value)
+
+    def datetime(self):
+        return datetime.datetime.strptime(self[:18], '%Y%m%d-%H%M%S-%f')
+
+    def uuid(self):
+        return self[20:]
+
+    @classmethod
+    def is_valid(cls, tuid):
+        """
+        A valid tuid is a string formatted like "YYYYMMDD-HHMMSS-fff-******".
+        """
+
+        if not tuid[:8].isdigit():
+            raise ValueError('Invalid timespec {}'.format(tuid))
+        if not tuid[9:15].isdigit():
+            raise ValueError('Invalid timespec {}'.format(tuid))
+        if not tuid[16:18].isdigit():
+            raise ValueError('Invalid timespec {}'.format(tuid))
+        if not tuid[8] == '-' and not tuid[15] == '-' and not tuid[19] == '-':
+            raise ValueError('Invalid timespec {}'.format(tuid))
+        if not len(tuid) == 26:
+            raise ValueError('Invalid uuid {}'.format(tuid))
