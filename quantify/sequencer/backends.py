e230dd18bf69d5ba809b0c47698dd1d3cfcc8684
Adriaan Rol <adriaan.rol@gmail.com>
2020-06-03 15:04:32 +0200

    prototype plotly pulse diagram

--- quantify/sequencer/backends.py
+++ quantify/sequencer/backends.py
@@ -16,6 +16,11 @@ from quantify.sequencer.waveforms import modulate_wave
 from matplotlib.cm import get_cmap
 
 
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
 def circuit_diagram_matplotlib(schedule, figsize=None):
     """
     Creates a circuit diagram visualization of a schedule using matplotlib.
@@ -140,3 +145,86 @@ def pulse_diagram_matplotlib(schedule, figsize=None,
 
     ax.legend(loc=(1.05, .5))
     return f, ax
+
+
+def pulse_diagram_plotly(schedule,
+                         ch_map: dict = None,
+                         fig_ch_height=100,
+                         modulation: bool = True,
+                         sampling_rate: float = 2e9):
+    """
+    Produce a visualization of the pulses used.
+    """
+
+    nr_rows = 10
+    fig = make_subplots(rows=nr_rows, cols=1, shared_xaxes=True,
+                        vertical_spacing=0.01)
+    fig.update_layout(
+        height=fig_ch_height*nr_rows, width=1000,
+        title=schedule.data['name'])
+
+    if ch_map is None:
+        auto_map = True
+        offset_idx = 0
+        ch_map = {}
+    else:
+        auto_map = False
+
+    colors = px.colors.qualitative.Plotly
+    col_idx = 0
+
+    for pls_idx, t_constr in enumerate(schedule.timing_constraints):
+        op = schedule.operations[t_constr['operation_hash']]
+
+        for p in op['pulse_info']:
+
+            # iterate through the colors in the color map
+            col_idx = (col_idx+1) % len(colors)
+
+            # times at which to evaluate waveform
+            t0 = t_constr['abs_time']+p['t0']
+            t = np.arange(t0, t0+p['duration'], 1/sampling_rate)
+
+            # function to generate waveform
+            if p['wf_func'] != None:
+                wf_func = import_func_from_string(p['wf_func'])
+
+                # select the arguments for the waveform function that are present in pulse info
+                par_map = inspect.signature(wf_func).parameters
+                wf_kwargs = {}
+                for kw in par_map.keys():
+                    if kw in p.keys():
+                        wf_kwargs[kw] = p[kw]
+                # Calculate the numerical waveform using the wf_func
+                wfs = wf_func(t=t, **wf_kwargs)
+
+                # optionally adds some modulation
+                if modulation and 'freq_mod' in p.keys():
+                    # apply modulation to the waveforms
+                    wfs = modulate_wave(
+                        t, wfs[0], wfs[1], p['freq_mod'])
+
+                for i, ch in enumerate(p['channels']):
+                    if ch not in ch_map.keys() and auto_map:
+                        ch_map[ch] = offset_idx
+                        offset_idx += 1
+
+                    # Ensures that the different parts of the same pulse are coupled to the same legend group.
+                    showlegend = (i == 0)
+                    label = op['name']
+                    fig.add_trace(go.Scatter(x=t, y=wfs[i], mode='lines', name=label, legendgroup=pls_idx,
+                                             showlegend=showlegend,
+                                             line_color=colors[col_idx]),
+                                  row=ch_map[ch]+1, col=1)
+
+    for r in range(nr_rows):
+        title = ''
+        if r+1 == nr_rows:
+            title = 'Time'
+        # FIXME: units are hardcoded
+        fig.update_xaxes(row=r+1, col=1, tickformat=".2s",
+                         hoverformat='.3s', ticksuffix='s', title=title)
+        fig.update_yaxes(row=r+1, col=1, tickformat=".2s", hoverformat='.3s',
+                         ticksuffix='V', title='Amplitude', range=[-1.1, 1.1])
+
+    return fig
