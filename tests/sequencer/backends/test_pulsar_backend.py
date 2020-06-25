d2f75edf053ffd786553daba3b101ed602d415fd
CallumAttryde <callumattryde@gmail.com>
2020-06-25 11:40:51 +0200

    add test for generate_sequencer_cfg

--- tests/sequencer/backends/test_pulsar_backend.py
+++ tests/sequencer/backends/test_pulsar_backend.py
@@ -2,7 +2,7 @@ import pathlib
 import numpy as np
 from quantify.sequencer.types import Schedule
 from quantify.sequencer.gate_library import Reset, Measure, CNOT, Rxy
-from quantify.sequencer.backends.pulsar_backend import prepare_waveforms_for_q1asm, construct_q1asm_pulse_operations
+from quantify.sequencer.backends.pulsar_backend import prepare_waveforms_for_q1asm, construct_q1asm_pulse_operations, generate_sequencer_cfg
 from quantify.sequencer.pulse_library import SquarePulse, DRAGPulse
 from quantify.sequencer.resources import QubitResource, CompositeResource, Pulsar_QCM_sequencer
 
@@ -38,6 +38,26 @@ def test_construct_q1asm_pulse_operations():
     program_str = construct_q1asm_pulse_operations(pulse_timings, pulse_data)
     with open(pathlib.Path(__file__).parent.joinpath('ref_test_construct_q1asm_pulse_operations'), 'rb') as f:
         assert program_str.encode('utf-8') == f.read()
+
+
+def test_generate_sequencer_cfg():
+    pulse_timings = [
+        (0, SquarePulse(amp=1.0, duration=4, ch='ch1')),
+        (4, DRAGPulse(G_amp=.8, D_amp=-.3, phase=24.3, duration=4, freq_mod=15e6, ch_I='ch1', ch_Q='ch2')),
+        (16, SquarePulse(amp=2.0, duration=4, ch='ch1')),
+    ]
+
+    pulse_data = {
+        pulse_timings[0][1].hash: [0, 1, 0],
+        pulse_timings[1][1].hash: [-1, 0, -1],
+        pulse_timings[2][1].hash: [-1, 1, -1],
+    }
+
+    sequence_cfg = generate_sequencer_cfg(pulse_data, pulse_timings)
+    assert sequence_cfg['waveforms'][pulse_timings[0][1].hash] == {'data': [0, 1, 0], 'index': 0}
+    assert sequence_cfg['waveforms'][pulse_timings[1][1].hash] == {'data': [-1, 0, -1], 'index': 1}
+    assert sequence_cfg['waveforms'][pulse_timings[2][1].hash] == {'data': [-1, 1, -1], 'index': 2}
+    assert len(sequence_cfg['program'])
 
 
 def test_pulsar_assembler_backend():
