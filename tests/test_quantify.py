8bd7c34eb71360c30afb907d1ac4360a37dbe1e0
CallumAttryde <callumattryde@gmail.com>
2020-08-04 12:12:43 +0200

    add copyright checking test

--- tests/test_quantify.py
+++ tests/test_quantify.py
@@ -1,24 +1,30 @@
-#!/usr/bin/env python
-
-"""Tests for `quantify` package."""
-
 import pytest
+import os
+import re
+import datetime
+import pprint
+from pathlib import Path
 
 
-# from quantify import quantify
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_copyrights():
+    skip_files = {'__init__.py'}
+    failures = {}
+    quantify_path = Path(__file__).resolve().parent / os.path.join("..", "quantify")
+    current_year = str(datetime.datetime.now().year)
+    for root, dirs, files in os.walk(quantify_path):
+        path = os.path.relpath(root, quantify_path).split(os.sep)
+        for file in files:
+            if file[-3:] == '.py' and file not in skip_files:
+                filepath = os.path.join(*path, file)
+                with open(os.path.join(quantify_path, filepath), 'r') as f:
+                    copyright_found = False
+                    for line in f.readlines(512):  # no need to read the whole file, this should be enough
+                        if re.match(".*Copyright.*Qblox BV", line):
+                            copyright_found = True
+                            if current_year not in line:
+                                failures[filepath] = "No copyright claim for {}.".format(current_year)
+                            break
+                    if not copyright_found:
+                        failures[filepath] = "No copyright found."
+    if failures:
+        pytest.fail("Bad copyrights:\n{}".format(pprint.pformat(failures)))
