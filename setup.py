50c6628f255e2aa3b1531da040ba5dd52a3aa496
Adriaan Rol <adriaan.rol@gmail.com>
2020-10-16 14:23:02 +0200

    updated setup.py with version license and author

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@
 
 from setuptools import setup, find_packages
 
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGELOG.rst') as history_file:
     history = history_file.read()
 
 with open('AUTHORS.rst') as authors_file:
@@ -34,15 +38,13 @@ requirements = [
 
 setup_requirements = ['pytest-runner', ]
 
-version = '0.1.1'
-
 setup(
-    author="QBlox BV",
+    author="The Quantify consortium consisting of Qblox and Orange Quantum Systems",
     python_requires='>=3.5',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD-4 License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
@@ -50,7 +52,7 @@ setup(
     ],
     description="Unified quantum computing, solid-state and pulse sequencing physical experimentation framework.",
     install_requires=requirements,
-    license="MIT license",
+    license="BSD-4 license",
     long_description=readme + '\n\n' + authors + '\n\n' + history,
     include_package_data=True,
     keywords='quantify-core',
@@ -60,7 +62,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://gitlab.com/qblox/packages/software/quantify',
+    url='https://https://gitlab.com/quantify-os/quantify-core',
     version='0.2.0',
     zip_safe=False,
 )
