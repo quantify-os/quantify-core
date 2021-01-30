4e576b9cce8c95e31fbd67c24cbd56c4e4674756
caenrigen <vnegirneac@qblox.com>
2021-01-30 16:16:26 +0100

    copyright note

--- quantify/utilities/_tests_helpers.py
+++ quantify/utilities/_tests_helpers.py
@@ -1,3 +1,8 @@
+# -----------------------------------------------------------------------------
+# Description:  Helpers for testing quantify.
+# Repository:   https://gitlab.com/quantify-os/quantify-core
+# Copyright (C) Qblox BV & Orange Quantum Systems Holding BV (2020-2021)
+# -----------------------------------------------------------------------------
 import pathlib
 
 
