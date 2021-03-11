6f36054eadca0567c1e1ec5bf697f6ae7035bd25
Adam Lawrence <adam@orangeqs.com>
2021-03-11 15:46:52 +0100

    create function to guess the phase velocity based on the median difference betwee successive phases

--- tests/analysis/test_fitting_models.py
+++ tests/analysis/test_fitting_models.py
@@ -12,11 +12,13 @@ tuid_list = dh.get_tuids_containing("resonator_analysis_test")
 
 
 def test_phase_guess():
-	mod = fm.ResonatorModel()
-	dataset = dh.load_dataset(tuid=tuid_list[2])
-	freq = np.array(dataset["x0"])
-	S21 = np.array(dataset["y0"] * np.cos(np.deg2rad(dataset["y1"])) + 1j * dataset["y0"] * np.sin(np.deg2rad(dataset["y1"])))
-	(phi_0, phi_v) = mod.phase_guess(S21, freq)
+    mod = fm.ResonatorModel()
+    dataset = dh.load_dataset(tuid=tuid_list[2])
+    freq = np.array(dataset["x0"])
+    S21 = np.array(dataset["y0"] * np.cos(np.deg2rad(dataset["y1"])) + 1j * dataset["y0"] * np.sin(np.deg2rad(dataset["y1"])))
+    (phi_0, phi_v) = mod.phase_guess(S21, freq)
 
-	assert (phi_0 + np.pi)%2*np.pi == approx((354.631574 + np.pi)%2*np.pi)
-	assert phi_v == approx(-3.7840e-07) 
+    # We allow a certain tolerance on the accuracy of the guess, as this is only an intial imput for our fit
+    guess_tolerance = 0.2
+
+    assert phi_v == approx(-3.7840e-07, rel=guess_tolerance) 
