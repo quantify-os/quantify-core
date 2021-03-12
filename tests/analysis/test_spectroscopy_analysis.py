40a004e24c61f7a27265654595d8a90f7de31229
Adam Lawrence <adam@orangeqs.com>
2021-03-12 15:25:09 +0100

    set up spectroscopy analysis test to take multiple datafiles

--- tests/analysis/test_spectroscopy_analysis.py
+++ tests/analysis/test_spectroscopy_analysis.py
@@ -13,52 +13,72 @@ class TestResonatorSpectroscopyAnalysis:
     def setup_class(cls):
         dh.set_datadir(get_test_data_dir())
 
-        cls.tuid = "20210305-154735-413-142768"
-        cls.a_obj = sa.ResonatorSpectroscopyAnalysis(tuid=cls.tuid)
+        tuid_list = dh.get_tuids_containing(
+            "Resonator_id", t_start="20210305", t_stop="20210306"
+        )
+
+        cls.tuids = tuid_list
+        cls.a_objs = [sa.ResonatorSpectroscopyAnalysis(tuid=tuid) for tuid in cls.tuids]
+
+        cls.frs = [
+            4482627786.933104,
+            4482670162.566818,
+            4540287828.70407,
+            4576421867.293702,
+        ]
+        cls.Qls = [
+            4983.385483402395,
+            4192.005581230714,
+            4910.617635185228,
+            6437.377871269456,
+        ]
+        cls.Qes = [
+            6606.202849302761,
+            7317.398211359418,
+            5216.566199947343,
+            7759.742961176549,
+        ]
 
     @pytest.mark.skip(reason="blocked by #161, see `base_analisys.AnalysisSteps`")
     def test_raw_data_not_in_processed_dataset(self):
-        container = Path(dh.locate_experiment_container(self.tuid))
-        file_path = (
-            container
-            / "analysis_ResonatorSpectroscopyAnalysis"
-            / "processed_dataset.hdf5"
-        )
-        dataset = dh.load_dataset_from_path(file_path)
+        for tuid in self.tuids:
+            container = Path(dh.locate_experiment_container(tuid))
+            file_path = (
+                container
+                / "analysis_ResonatorSpectroscopyAnalysis"
+                / "processed_dataset.hdf5"
+            )
+            dataset = dh.load_dataset_from_path(file_path)
 
-        assert "x0" in dataset.dims.keys()
-        assert "x0" in dataset.coords.keys()
-        assert "y0" not in dataset.data_vars.keys()
-        assert "y1" not in dataset.data_vars.keys()
-        assert "S21" in dataset.data_vars.keys()
+            assert "x0" in dataset.dims.keys()
+            assert "x0" in dataset.coords.keys()
+            assert "y0" not in dataset.data_vars.keys()
+            assert "y1" not in dataset.data_vars.keys()
+            assert "S21" in dataset.data_vars.keys()
 
     def test_figures_generated(self):
         # test that the right figures get created.
-        assert set(self.a_obj.figs_mpl.keys()) == {
-            "S21-RealImag",
-            "S21-MagnPhase",
-            "S21-complex",
-        }
+        for a_obj in self.a_objs:
+            assert set(a_obj.figs_mpl.keys()) == {
+                "S21-RealImag",
+                "S21-MagnPhase",
+                "S21-complex",
+            }
 
     def test_quantities_of_interest(self):
-        assert set(self.a_obj.quantities_of_interest.keys()) == {
-            "Qi",
-            "Qe",
-            "Ql",
-            "Qc",
-            "fr",
-            "fit_res",
-            "fit_msg",
-        }
+        for a_obj, fr, Ql, Qe in zip(self.a_objs, self.frs, self.Qls, self.Qes):
+            assert set(a_obj.quantities_of_interest.keys()) == {
+                "Qi",
+                "Qe",
+                "Ql",
+                "Qc",
+                "fr",
+                "fit_res",
+                "fit_msg",
+            }
 
-        fitted_freq = self.a_obj.quantities_of_interest["fr"]
-        assert isinstance(fitted_freq, Variable)
-        assert self.a_obj.quantities_of_interest["fr"].nominal_value == approx(
-            4482670162.566818
-        )
-        assert self.a_obj.quantities_of_interest["Ql"].nominal_value == approx(
-            4192.00558
-        )
-        assert self.a_obj.quantities_of_interest["Qe"].nominal_value == approx(
-            7317.39821
-        )
+            fitted_freq = a_obj.quantities_of_interest["fr"]
+            assert isinstance(fitted_freq, Variable)
+            assert a_obj.quantities_of_interest["fr"].nominal_value == approx(fr)
+            assert a_obj.quantities_of_interest["Ql"].nominal_value == approx(Ql)
+            assert a_obj.quantities_of_interest["Qe"].nominal_value == approx(Qe)
