1e1017934e5b074d3d1f7f57992cd2a66f224fec
adamorangeqs <adam@orangeqs.com>
2021-04-15 22:13:57 +0200

    fixed codacy problems

--- quantify/utilities/experiment_helpers.py
+++ quantify/utilities/experiment_helpers.py
@@ -1,19 +1,21 @@
 # Repository: https://gitlab.com/quantify-os/quantify-core
 # Licensed according to the LICENCE file on the master branch
 """Helpers for performing experiments."""
+import warnings
 from qcodes import Instrument
 from quantify.data.types import TUID
 from quantify.data.handling import load_snapshot, get_latest_tuid
 from quantify.visualization.pyqt_plotmon import PlotMonitor_pyqt
-import warnings
 
 
 def load_settings_onto_instrument(
     instrument: Instrument, tuid: TUID = None, datadir: str = None
 ):
     """
-    Loads settings from a previous experiment onto a current :class:`~qcodes.instrument.base.Instrument`. This
-    information is loaded from the 'snapshot.json' file in the provided experiment directory.
+    Loads settings from a previous experiment onto a current
+    :class:`~qcodes.instrument.base.Instrument`. This information
+    is loaded from the 'snapshot.json' file in the provided experiment
+    directory.
 
     Parameters
     ----------
@@ -22,13 +24,14 @@ def load_settings_onto_instrument(
     tuid : :class:`~quantify.data.types.TUID`
         the TUID of the experiment. If None use latest TUID.
     datadir : str
-        path of the data directory. If `None`, uses `get_datadir()` to determine the data directory.
+        path of the data directory. If `None`, uses `get_datadir()` to
+        determine the data directory.
     Raises
     ------
     ValueError
         if the provided instrument has no match in the loaded snapshot.
     """
-    if tuid == None:
+    if tuid is None:
         tuid = get_latest_tuid()
 
     instruments = load_snapshot(tuid, datadir)["instruments"]
@@ -45,22 +48,24 @@ def load_settings_onto_instrument(
             if "set" in dir(
                 instrument.__dict__["parameters"][parname]
             ):  # Make sure the parameter is actually a settable
-                val = par["value"]
                 try:
                     instrument.set(parname, par["value"])
                 except Exception as exp:
                     warnings.warn(
-                        f"Parameter {parname} of instrument {instrument.name} could not be set to {val} due to error:\n{exp}"
+                        f"Parameter {parname} of instrument {instrument.name} could "
+                        "not be set to {val} due to error:\n{exp}"
                     )
         else:
             warnings.warn(
-                f"{instrument.name} does not possess a parameter {parname}. Could not set parameter."
+                f"{instrument.name} does not possess a parameter {parname}. Could not "
+                "set parameter."
             )
 
 
 def create_plotmon_from_historical(tuid: TUID):
     """
-    Creates a plotmon using the dataset of the provided experiment denoted by the tuid in the datadir.
+    Creates a plotmon using the dataset of the provided experiment denoted by the tuid
+    in the datadir.
     Loads the data and draws any required figures.
 
     Parameters
