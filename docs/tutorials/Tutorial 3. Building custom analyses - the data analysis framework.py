1c0f0266fb16001ef4e30019c7c17a87f7e09642
caenrigen <vnegirneac@qblox.com>
2021-05-05 17:31:09 +0200

    wip

--- docs/tutorials/Tutorial 3. Building custom analyses - the data analysis framework.py
+++ docs/tutorials/Tutorial 3. Building custom analyses - the data analysis framework.py
@@ -129,11 +129,6 @@ dataset = load_dataset(tuid)
 dataset
 
 
-# %% [markdown]
-# ## TO DO
-#
-# - [ ] use the object orinted interface for lmfit from the beggining
-
 # %%
 
 fitting_model = lmfit.Model(
@@ -218,7 +213,11 @@ def run_fitting(dataset: xr.Dataset) -> lmfit.model.ModelResult:
     return result
 
 
-def extract_quantities_of_interst(fit_result: lmfit.model.ModelResult) -> dict:
+def analyze_fit_results(fit_result: lmfit.model.ModelResult) -> dict:
+    # analyze results
+    # ...
+
+    # extract relevant quantities
     quantities = {
         "amplitude": fit_result.params["amplitude"].value,
         "frequency": fit_result.params["frequency"].value,
@@ -229,7 +228,7 @@ def extract_quantities_of_interst(fit_result: lmfit.model.ModelResult) -> dict:
 # %%
 dataset = extract_data(label="Cosine experiment")
 fit_result = run_fitting(dataset=dataset)
-quantities_of_interst = extract_quantities_of_interst(fit_result=fit_result)
+quantities_of_interst = analyze_fit_results(fit_result=fit_result)
 # as expected we obtain the same results
 _ = fit_result.plot_fit(show_init=True)
 print(f"quantities_of_interst: {quantities_of_interst}")
@@ -332,6 +331,7 @@ def save_quantities_of_interst(tuid: str, quantities_of_interst: dict) -> None:
 def save_mpl_figure(tuid: str, fig: matplotlib.figure.Figure) -> None:
     exp_folder = Path(locate_experiment_container(tuid))
     fig.savefig(exp_folder / "Cosine fit.png", dpi=300, bbox_inches="tight")
+    plt.close(fig)
 
 
 # %%
@@ -354,7 +354,7 @@ dataset = extract_data(label="Cosine experiment")
 fit_result = run_fitting(dataset)
 
 # extract quantities of interest
-quantities_of_interst = extract_quantities_of_interst(fit_result)
+quantities_of_interst = analyze_fit_results(fit_result)
 
 # plot results and fit
 fig, ax = plt.subplots()
@@ -367,10 +367,7 @@ save_mpl_figure(dataset.tuid, fig)
 # %%
 from directory_tree import display_tree
 
-print(
-    display_tree(Path(locate_experiment_container(dataset.tuid)), string_rep=True),
-    end="",
-)
+print(display_tree(Path(locate_experiment_container(dataset.tuid)), string_rep=True))
 
 # %% [markdown]
 # ### Class approach
@@ -386,11 +383,11 @@ from collections import OrderedDict
 
 class MyCosineAnalysis:
     def __init__(self, label: str):
-        """This is a special method that python calls when a an instance of this class is created."""
+        """This is a special method that python calls when an instance of this class is created."""
 
-        self.label = None
+        self.label = label
         self.tuid = None
-        self.raw_dataset = None
+        self.dataset_raw = None
 
         # objects to be filled up later when running the analysis
         self.fit_results = OrderedDict()
@@ -400,22 +397,31 @@ class MyCosineAnalysis:
 
     # with just slight modification our functions become methods
     # with the advantage that we have access to all the necessary information from `self.`
+    def run(self):
+        """Execute the analysis steps"""
+        self.extract_data()
+        self.run_fitting()
+        self.analyze_fit_results()
+        self.create_figures()
+        self.save_quantities_of_interst()
+        self.save_figures()
+
+        return self
 
     def extract_data(self):
         self.tuid = get_latest_tuid(contains=self.label)
-        self.raw_dataset = load_dataset(tuid)
+        self.dataset_raw = load_dataset(tuid)
 
     def run_fitting(self):
-        self.fitting_model = lmfit.Model(
-            cos_func
-        )  # create a fitting model based on a cosine function
-        guess = guess_paramaters(self.fitting_model)
+        """Fits a CosineModel to the data."""
+        model = CosineModel()
+        guess = model.guess(self.dataset_raw.y0.values)
         result = model.fit(
-            self.raw_dataset.y0.values, x=self.raw_dataset.x0.values, params=guess
+            self.dataset_raw.y0.values, x=self.dataset_raw.x0.values, params=guess
         )
-        self.fit_result.update({"cosine": result})
+        self.fit_results.update({"cosine": result})
 
-    def extract_quantities_of_interst(self):
+    def analyze_fit_results(self):
         self.quantities_of_interst.update(
             {
                 "amplitude": self.fit_results["cosine"].params["amplitude"].value,
@@ -430,20 +436,20 @@ class MyCosineAnalysis:
 
     def plot_fit(self, fig: matplotlib.figure.Figure, ax: matplotlib.axes.Axes):
         # plot data
-        self.raw_dataset.y0.plot.line(ax=ax, x="x0", marker="o", label="Data")
+        self.dataset_raw.y0.plot.line(ax=ax, x="x0", marker="o", label="Data")
 
         # plot fit
-        x_fit = np.linspace(self.raw_dataset["x0"][0], self.raw_dataset["x0"][-1], 1000)
+        x_fit = np.linspace(self.dataset_raw["x0"][0], self.dataset_raw["x0"][-1], 1000)
         y_fit = cos_func(x=x_fit, **fit_result.best_values)
         ax.plot(x_fit, y_fit, label="Fit")
         ax.legend()
 
         # set units-aware tick labels
         set_xlabel(
-            ax, self.raw_dataset.x0.long_name, self.raw_dataset.x0.attrs["units"]
+            ax, self.dataset_raw.x0.long_name, self.dataset_raw.x0.attrs["units"]
         )
         set_ylabel(
-            ax, self.raw_dataset.y0.long_name, self.raw_dataset.y0.attrs["units"]
+            ax, self.dataset_raw.y0.long_name, self.dataset_raw.y0.attrs["units"]
         )
 
         # add a reference to the origal dataset in the figure title
@@ -453,16 +459,140 @@ class MyCosineAnalysis:
         fig, ax = plt.subplots()
         self.plot_fit(fig, ax)
 
-        fig_name = ax_name = "Cosine data and fit"
-        self.figs_mpl.update({fig_name: fig})
+        fig_id = "cos-data-and-fit"
+        self.figs_mpl.update({fig_id: fig})
         # keep a reference to `ax` as well
         # it can be accessed later to apply modifications (e.g., in a notebook)
-        self.figs_axs.update({ax_name: ax})
+        self.axs_mpl.update({fig_id: ax})
 
-    def save_mpl_figures():
+    def save_figures(self):
         exp_folder = Path(locate_experiment_container(self.tuid))
         for fig_name, fig in self.figs_mpl.items():
             fig.savefig(exp_folder / f"{fig_name}.png", dpi=300, bbox_inches="tight")
+            plt.close(fig)
 
+
+# %%
+a_obj = MyCosineAnalysis(
+    label="Cosine experiment"
+)  # calls MyCosineAnalysis.__init__(...)
+a_obj.run()
+
+# %%
+print(
+    display_tree(
+        Path(locate_experiment_container(a_obj.dataset_raw.tuid)), string_rep=True
+    )
+)
+
+# %% [markdown]
+# ## Using the BaseAnalysis class
+
+# %% [raw]
+# import quantify.analysis.base_analysis as ba
+# from quantify.visualization import mpl_plotting as qpl
+# from quantify.visualization.SI_utilities import format_value_string, adjust_axeslabels_SI
+#
+# # extend the BaseAnalysis
+# class CosineAnalysis(ba.BaseAnalysis):
+#
+#     def process_data(self):
+#         """
+#         In some cases, you might need to process the data, e.g., reshape, filter etc.,
+#         before starting the analysis. This is the method where it should be done.
+#
+#         For examples see :meth:`~quantify.analysis.spectroscopy_analysis.ResonatorSpectroscopyAnalysis.process_dataset`
+#         """
+#
+#     def run_fitting(self):
+#         # create a fitting model based on a cosine function
+#         model = CosineModel()
+#         guess = model.guess(self.dataset_raw.y0.values)
+#         result = model.fit(
+#             self.dataset_raw.y0.values, x=self.dataset_raw.x0.values, params=guess
+#         )
+#         self.fit_results.update({"cosine": result})
+#
+#     def create_figures(self):
+#         fig, ax = plt.subplots()
+#         fig_id = "cos_fit"
+#         self.figs_mpl.update({fig_id: fig})
+#         self.axs_mpl.update({fig_id: ax})
+#
+#         self.dataset_raw.y0.plot(ax=ax, x="x0", marker="o", linestyle="")
+#         qpl.plot_fit(ax, self.fit_results["cosine"])
+#         qpl.plot_textbox(ax, ba.wrap_text(self.quantities_of_interest["fit_msg"]))
+#
+#         adjust_axeslabels_SI(ax)
+#         qpl.set_suptitle_from_dataset(fig, self.dataset_raw, f"x0-y0")
+#         ax.legend()
+#
+#     def analyze_fit_results(self):
+#         fit_res = self.fit_results["cosine"]
+#         fit_warning = ba.check_lmfit(fit_res)
+#
+#         # If there is a problem with the fit, display an error message in the text box.
+#         # Otherwise, display the parameters as normal.
+#         if fit_warning is None:
+#             self.quantities_of_interest["fit_success"] = True
+#             unit = self.dataset_raw.y0.units
+#             text_msg = "Summary\n"
+#             text_msg += format_value_string(
+#                 r"$f$", fit_res.params["frequency"], end_char="\n", unit="Hz"
+#             )
+#             text_msg += format_value_string(
+#                 r"$A$", fit_res.params["amplitude"], unit=unit
+#             )
+#         else:
+#             text_msg = fit_warning
+#             self.quantities_of_interest["fit_success"] = False
+#
+#         # save values and fit uncertainty
+#         for parname in ["frequency", "amplitude"]:
+#             self.quantities_of_interest[parname] = ba.lmfit_par_to_ufloat(
+#                 fit_res.params[parname]
+#             )
+#         self.quantities_of_interest["fit_msg"] = text_msg
+
+# %%
+MC.settables(pars.t)
+MC.setpoints(np.linspace(0, 2, 50))
+MC.gettables(pars.sig)
+dataset = MC.run("Cosine experiment")
+
+# %%
+from quantify.analysis.cosine_analysis import CosineAnalysis
+
+a_obj = CosineAnalysis(label="Cosine experiment").run()
+
+a_obj.display_figs_mpl()
+
+# %%
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# %%
+logger.info("bla")
+
+# %%
+a_obj.logger.setLevel("INFO")
+a_obj.run()
+
+# %%
+a_obj.logger.info("bla")
+
+# %%
+print(
+    display_tree(
+        Path(locate_experiment_container(a_obj.dataset_raw.tuid)), string_rep=True
+    )
+)
+
+# %%
+import logging
+
+logging.info("")  # call to info too early
 
 # %%
