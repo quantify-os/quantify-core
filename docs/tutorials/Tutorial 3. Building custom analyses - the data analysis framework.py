c6c30b181c49f79928dfcdd0bc3c8ffabc0e69ba
caenrigen <vnegirneac@qblox.com>
2021-05-05 13:57:08 +0200

    save wip

--- docs/tutorials/Tutorial 3. Building custom analyses - the data analysis framework.py
+++ docs/tutorials/Tutorial 3. Building custom analyses - the data analysis framework.py
@@ -7,7 +7,7 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
-# %matplotlib inline # automatically display matplolib figure created in each cell
+# %matplotlib inline
 
 
 # %%
@@ -111,7 +111,7 @@ MC.settables(pars.t)
 MC.setpoints(np.linspace(0, 2, 30))
 MC.gettables(pars.sig)
 dataset = MC.run("Cosine experiment")
-dataset
+plotmon.main_QtPlot
 
 # %% [markdown]
 # # Analyse
@@ -126,7 +126,6 @@ from quantify.data.handling import load_dataset, get_latest_tuid
 tuid = get_latest_tuid(contains="Cosine experiment")
 print("tuid: {}".format(tuid))
 dataset = load_dataset(tuid)
-dataset.y0.plot()  # quick preview
 dataset
 
 
@@ -144,8 +143,8 @@ fitting_model = lmfit.Model(
 # and specify initial guesses for each parameter
 fitting_model.set_param_hint("amplitude", value=0.5, min=0.1, max=2, vary=True)
 fitting_model.set_param_hint("frequency", value=0.8, vary=True)
-fitting_model.set_param_hint("phase", value=0, vary=False)
-fitting_model.set_param_hint("offset", value=0, vary=False)
+fitting_model.set_param_hint("phase", value=0)
+fitting_model.set_param_hint("offset", value=0)
 params = fitting_model.make_params()
 # and here we perform the fit.
 fit_result = fitting_model.fit(dataset.y0.values, x=dataset.x0.values, params=params)
@@ -169,34 +168,68 @@ quantities_of_interst
 # %% [markdown]
 # ### do a bit better, modulairty and reusbale
 
+# %% [markdown]
+# Object oriented interface of lmfit
+
 # %%
-def guess_cosine_paramaters(model: lmfit.Model) -> lmfit.parameter.Parameters:
-    # specify initial guesses for each parameter
-    model.set_param_hint("amplitude", value=0.5, min=0.1, max=2, vary=True)
-    model.set_param_hint("frequency", value=0.8, vary=True)
-    model.set_param_hint("phase", value=0, vary=False)
-    model.set_param_hint("offset", value=0, vary=False)
-    params = model.make_params()
-    return params
+class CosineModel(lmfit.model.Model):
+    """
+    lmfit model with a guess for a cosine fit.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # pass in the model's equation equation
+        super().__init__(cos_func, *args, **kwargs)
+
+        # configure constraints that are independent from the data to be fitted
+
+        self.set_param_hint("frequency", min=0, vary=True)  # enforce positive frequency
+        self.set_param_hint("amplitude", min=0, vary=True)  # enforce positive amplitude
+        self.set_param_hint("offset", vary=True)
+        self.set_param_hint(
+            "phase", vary=True, min=-np.pi, max=np.pi
+        )  # enforce phase range
+
+    def guess(self, data, **kws) -> lmfit.parameter.Parameters:
+
+        # guess parameters based on the data
+
+        self.set_param_hint("offset", value=np.average(data))
+        self.set_param_hint("amplitude", value=(np.max(data) - np.min(data)) / 2)
+        # a simple educated guess based on experiment type
+        # a more elaborate but general approach is to use a Fourier transform
+        self.set_param_hint("frequency", value=1.2)
+
+        params = self.make_params()
+        return lmfit.models.update_param_vals(params, self.prefix, **kws)
 
 
-def fit_cosine(dataset: xr.Dataset) -> lmfit.model.ModelResult:
-    model = lmfit.Model(cos_func)  # create a fitting model based on a cosine function
-    params = guess_cosine_paramaters(model)
-    result = model.fit(dataset.y0.values, x=dataset.x0.values, params=params)
+# %%
+def extract_data(label: str) -> xr.Dataset:
+    tuid = get_latest_tuid(contains=label)
+    dataset = load_dataset(tuid)
+    return dataset
+
+
+def run_fitting(dataset: xr.Dataset) -> lmfit.model.ModelResult:
+    model = CosineModel()  # create the fitting model
+    params_guess = model.guess(data=dataset.y0.values)
+    result = model.fit(data=dataset.y0.values, x=dataset.x0.values, params=params_guess)
     return result
 
 
-def extract_quantities_of_interst(cosine_fit_result: lmfit.model.ModelResult) -> dict:
+def extract_quantities_of_interst(fit_result: lmfit.model.ModelResult) -> dict:
     quantities = {
-        "amplitude": cosine_fit_result.params["amplitude"].value,
-        "frequency": cosine_fit_result.params["frequency"].value,
+        "amplitude": fit_result.params["amplitude"].value,
+        "frequency": fit_result.params["frequency"].value,
     }
     return quantities
 
 
-fit_result = fit_cosine(dataset)
-quantities_of_interst = extract_quantities_of_interst(fit_result)
+# %%
+dataset = extract_data(label="Cosine experiment")
+fit_result = run_fitting(dataset=dataset)
+quantities_of_interst = extract_quantities_of_interst(fit_result=fit_result)
 # as expected we obtain the same results
 _ = fit_result.plot_fit(show_init=True)
 print(f"quantities_of_interst: {quantities_of_interst}")
@@ -216,59 +249,53 @@ dataset.y0.plot.line(ax=ax, x="x0", marker="o", label="Data")
 
 # plot fit
 x_fit = np.linspace(dataset["x0"][0], dataset["x0"][-1], 1000)
-y_fit = cos_func(x=x_fit, **fit_res.best_values)
+y_fit = cos_func(x=x_fit, **fit_result.best_values)
 ax.plot(x_fit, y_fit, label="Fit")
 ax.legend()
 
 # set units-aware tick labels
-set_xlabel(ax, dataset.x0.attrs["long_name"], dataset.x0.attrs["units"])
-set_ylabel(ax, dataset.y0.attrs["long_name"], dataset.y0.attrs["units"])
+set_xlabel(ax, dataset.x0.long_name, dataset.x0.units)
+set_ylabel(ax, dataset.y0.long_name, dataset.y0.units)
 
 # add a reference to the origal dataset in the figure title
-_ = fig.suptitle(f"{dataset.attrs['name']}\n" f"{dataset.attrs['tuid']}")
+_ = fig.suptitle(f"{dataset.attrs['name']}\ntuid: {dataset.attrs['tuid']}")
 
 
 # %% [markdown]
-# Again this can be packed into a reusable function
+# Again, this can be packed into a reusable function
 
 # %%
 import matplotlib
 from typing import Tuple
 
 
-def plot_cosine_fit(
+def plot_fit(
+    fig: matplotlib.figure.Figure,
+    ax: matplotlib.axes.Axes,
     dataset: xr.Dataset,
     fit_result: lmfit.model.ModelResult,
-    fig: matplotlib.figure.Figure = None,
-    ax: matplotlib.axes.Axes = None,
 ) -> Tuple[matplotlib.figure.Figure, matplotlib.axes.Axes]:
-
-    if fig is None or ax is None:
-        # create matplotlib figure
-        fig, ax = plt.subplots()
 
     # plot data
     dataset.y0.plot.line(ax=ax, x="x0", marker="o", label="Data")
 
     # plot fit
     x_fit = np.linspace(dataset["x0"][0], dataset["x0"][-1], 1000)
-    y_fit = cos_func(x=x_fit, **fit_res.best_values)
+    y_fit = cos_func(x=x_fit, **fit_result.best_values)
     ax.plot(x_fit, y_fit, label="Fit")
     ax.legend()
 
     # set units-aware tick labels
-    set_xlabel(ax, dataset.x0.attrs["long_name"], dataset.x0.attrs["units"])
-    set_ylabel(ax, dataset.y0.attrs["long_name"], dataset.y0.attrs["units"])
+    set_xlabel(ax, dataset.x0.long_name, dataset.x0.units)
+    set_ylabel(ax, dataset.y0.long_name, dataset.y0.units)
 
     # add a reference to the origal dataset in the figure title
-    fig.suptitle(f"{dataset.attrs['name']}\n" f"{dataset.attrs['tuid']}")
-
-    return fig, ax
+    fig.suptitle(f"{dataset.attrs['name']}\ntuid: {dataset.attrs['tuid']}")
 
 
-fig, ax = plot_cosine_fit(dataset, fit_result)
-
-# %%
+fig, ax = plt.subplots()
+# same output as expected:
+plot_fit(fig=fig, ax=ax, dataset=dataset, fit_result=fit_result)
 
 # %%
 from quantify.data.handling import locate_experiment_container
@@ -284,7 +311,7 @@ exp_folder
 import json
 
 # Save fit results
-with open(exp_folder / "quantities_of_interst_cosine.json", "w") as file:
+with open(exp_folder / "quantities_of_interst.json", "w") as file:
     json.dump(quantities_of_interst, file)
 
 # Save figure
@@ -295,46 +322,47 @@ fig.savefig(exp_folder / "Cosine fit.png", dpi=300, bbox_inches="tight")
 # Again... tedious...
 
 # %%
-def save_quantities_of_interst_cosine(tuid: str, quantities_of_interst: dict) -> None:
+def save_quantities_of_interst(tuid: str, quantities_of_interst: dict) -> None:
     exp_folder = Path(locate_experiment_container(tuid))
     # Save fit results
-    with open(exp_folder / "quantities_of_interst_cosine.json", "w") as file:
+    with open(exp_folder / "quantities_of_interst.json", "w") as file:
         json.dump(quantities_of_interst, file)
 
 
-save_quantities_of_interst_cosine(dataset.tuid, quantities_of_interst)
-
-
-# %%
-def save_mpl_figure_cosine(tuid: str, fig: matplotlib.figure.Figure) -> None:
+def save_mpl_figure(tuid: str, fig: matplotlib.figure.Figure) -> None:
     exp_folder = Path(locate_experiment_container(tuid))
     fig.savefig(exp_folder / "Cosine fit.png", dpi=300, bbox_inches="tight")
 
 
-save_mpl_figure_cosine(dataset.tuid, fig)
+# %%
+save_quantities_of_interst(dataset.tuid, quantities_of_interst)
+save_mpl_figure(dataset.tuid, fig)
 
 # %% [markdown]
 # ### Runnning the full analysis using the functions
+#
+# We can already a recognize a certain common flow
 
 # %%
 # load data
-dataset = load_dataset(get_latest_tuid(contains="Cosine experiment"))
+dataset = extract_data(label="Cosine experiment")
 
 # typically the function would be imported form a module
-# from my_module import fit_cosine, plot_cosine_fit, save_quantities_of_interst_cosine, ...
+# from my_module import fit, plot_fit, save_quantities_of_interst, ...
 
 # run fitting
-fit_result = fit_cosine(dataset)
+fit_result = run_fitting(dataset)
 
 # extract quantities of interest
 quantities_of_interst = extract_quantities_of_interst(fit_result)
 
 # plot results and fit
-fig, ax = plot_cosine_fit(dataset, fit_result)
+fig, ax = plt.subplots()
+plot_fit(fig, ax, dataset, fit_result)
 
 # save analysis results and figures to disk
-save_quantities_of_interst_cosine(dataset.tuid, quantities_of_interst)
-save_mpl_figure_cosine(dataset.tuid, fig)
+save_quantities_of_interst(dataset.tuid, quantities_of_interst)
+save_mpl_figure(dataset.tuid, fig)
 
 # %%
 from directory_tree import display_tree
@@ -344,49 +372,97 @@ print(
     end="",
 )
 
-
 # %% [markdown]
 # ### Class approach
 #
 # Best practices:
-# - **do not add** any attributes to the analysis object after its initial creation and configuration (e.g., `self.my_new_attr = [1, 2, 3]` in the `fit_cosine` method is a bad practice, instead create the attribute before running the analysis as `self.my_new_attr = None`). This avoids common pitfalls of the objected-oriented-programming that can be hard to debbug later
+# - **do not add** any attributes to the analysis object after its initial creation and configuration (e.g., `self.my_new_attr = [1, 2, 3]` in the `fit` method is a bad practice, instead create the attribute before running the analysis as `self.my_new_attr = None`). This avoids common pitfalls of the objected-oriented-programming that can be hard to debbug later
 # - do no modify the raw dataset
 # - split your code in multiple methods (functions) that execute specific tasks
 
 # %%
+from collections import OrderedDict
+
+
 class MyCosineAnalysis:
     def __init__(self, label: str):
         """This is a special method that python calls when a an instance of this class is created."""
-        self.raw_dataset = load_dataset(get_latest_tuid(contains=label))
-        self.tuid = self.raw_dataset.tuid
+
+        self.label = None
+        self.tuid = None
+        self.raw_dataset = None
 
         # objects to be filled up later when running the analysis
-        self.fit_result = None
-        self.fit_model = None
-        self.fit_params = None
-        self.quantities_of_interst = None
-        self.fig = None
-        self.ax = None
+        self.fit_results = OrderedDict()
+        self.quantities_of_interst = OrderedDict()
+        self.figs_mpl = OrderedDict()
+        self.axs_mpl = OrderedDict()
 
     # with just slight modification our functions become methods
     # with the advantage that we have access to all the necessary information from `self.`
 
-    def fit_cosine(self):
+    def extract_data(self):
+        self.tuid = get_latest_tuid(contains=self.label)
+        self.raw_dataset = load_dataset(tuid)
+
+    def run_fitting(self):
         self.fitting_model = lmfit.Model(
             cos_func
         )  # create a fitting model based on a cosine function
-        self.params = guess_cosine_paramaters(self.fitting_model)
-        self.result = model.fit(
-            dataset.y0.values, x=dataset.x0.values, params=self.params
+        guess = guess_paramaters(self.fitting_model)
+        result = model.fit(
+            self.raw_dataset.y0.values, x=self.raw_dataset.x0.values, params=guess
+        )
+        self.fit_result.update({"cosine": result})
+
+    def extract_quantities_of_interst(self):
+        self.quantities_of_interst.update(
+            {
+                "amplitude": self.fit_results["cosine"].params["amplitude"].value,
+                "frequency": self.fit_results["cosine"].params["frequency"].value,
+            }
         )
 
-    def extract_quantities_of_interst(
-        cosine_fit_result: lmfit.model.ModelResult,
-    ) -> dict:
-        self.quantities_of_interst = {
-            "amplitude": cosine_fit_result.params["amplitude"].value,
-            "frequency": cosine_fit_result.params["frequency"].value,
-        }
+    def save_quantities_of_interst(self):
+        exp_folder = Path(locate_experiment_container(self.tuid))
+        with open(exp_folder / "quantities_of_interst.json", "w") as file:
+            json.dump(self.quantities_of_interst, file)
+
+    def plot_fit(self, fig: matplotlib.figure.Figure, ax: matplotlib.axes.Axes):
+        # plot data
+        self.raw_dataset.y0.plot.line(ax=ax, x="x0", marker="o", label="Data")
+
+        # plot fit
+        x_fit = np.linspace(self.raw_dataset["x0"][0], self.raw_dataset["x0"][-1], 1000)
+        y_fit = cos_func(x=x_fit, **fit_result.best_values)
+        ax.plot(x_fit, y_fit, label="Fit")
+        ax.legend()
+
+        # set units-aware tick labels
+        set_xlabel(
+            ax, self.raw_dataset.x0.long_name, self.raw_dataset.x0.attrs["units"]
+        )
+        set_ylabel(
+            ax, self.raw_dataset.y0.long_name, self.raw_dataset.y0.attrs["units"]
+        )
+
+        # add a reference to the origal dataset in the figure title
+        fig.suptitle(f"{dataset.attrs['name']}\ntuid: {dataset.attrs['tuid']}")
+
+    def create_figures(self):
+        fig, ax = plt.subplots()
+        self.plot_fit(fig, ax)
+
+        fig_name = ax_name = "Cosine data and fit"
+        self.figs_mpl.update({fig_name: fig})
+        # keep a reference to `ax` as well
+        # it can be accessed later to apply modifications (e.g., in a notebook)
+        self.figs_axs.update({ax_name: ax})
+
+    def save_mpl_figures():
+        exp_folder = Path(locate_experiment_container(self.tuid))
+        for fig_name, fig in self.figs_mpl.items():
+            fig.savefig(exp_folder / f"{fig_name}.png", dpi=300, bbox_inches="tight")
 
 
 # %%
