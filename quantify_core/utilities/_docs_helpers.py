6c0e8a4ca46e79c5eb16241f1f2650d36f70ff8a
caenrigen <vnegirneac@qblox.com>
2021-08-04 20:00:40 +0200

    add utility for generating tutorial rst from notebook

--- quantify_core/utilities/_docs_helpers.py
+++ quantify_core/utilities/_docs_helpers.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 from typing import Union
+import json
 import shutil
 import tempfile
 from pathlib import Path
@@ -53,3 +54,61 @@ def create_tmp_dir_from_test_dataset(tuid: Union[TUID, str]):
     )
 
     return tmp_path, tmp_dir
+
+
+def notebook_to_rst(notebook_filepath: Path, output_filepath: Path) -> None:
+    """
+    An experimental utility to covert a Jupyter notebook into an .rst file for writing
+    tutorials.
+
+    Intended usage:
+
+    - Use jupytext to pair a .ipynb notebook with a percent script.
+    - Version control the .source.py percent script version of the notebook.
+    - Generate a `.rst` file with this script to make it part of the docs.
+    """
+
+    def get_code_indent(code_cell_source):
+        indent = ""
+        for line in code_cell_source:
+            if line == "\n":
+                continue
+
+            for char in line:
+                if char == " ":
+                    indent += char
+                else:
+                    break
+            break
+        return indent
+
+    def make_jupyter_sphinx_block(cell_source):
+        indent = get_code_indent(cell_source)
+        out = f"\n\n\n{indent}.. jupyter-execute::\n\n"
+        indent = f"    {indent}"
+        for line in cell_source:
+            out += f"{indent}{line}" if line != "\n" else "\n"
+
+        return out
+
+    def make_rst_block(cell_source):
+        return "\n\n\n" + "".join(cell_source)
+
+    def cell_to_rst_str(cell):
+        cell_type = cell["cell_type"]
+        cell_source = cell["source"]
+
+        if cell_type == "code":
+            return make_jupyter_sphinx_block(cell_source)
+
+        return make_rst_block(cell_source)
+
+    with open(Path(notebook_filepath), "r") as file:
+        json_dict = json.load(file)
+
+    rst_str = ""
+    for cell in json_dict["cells"]:
+        rst_str += cell_to_rst_str(cell)
+
+    with open(Path(output_filepath), "w") as file:
+        file.write(rst_str)
