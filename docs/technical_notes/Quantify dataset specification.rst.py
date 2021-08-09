8aec054b0953aa006c013c3f9c46dcb629d87de1
caenrigen <vnegirneac@qblox.com>
2021-08-09 19:04:32 +0200

    wip T1 datasets

--- docs/technical_notes/Quantify dataset specification.rst.py
+++ docs/technical_notes/Quantify dataset specification.rst.py
@@ -432,6 +432,7 @@ dataset.quantify_dataset_version, dataset.tuid
 # %%
 dataset_2d_example.x0.attrs, dataset_2d_example.x0.standard_name
 
+
 # %% [raw]
 # Calibration variables and dimensions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -460,3 +461,351 @@ dataset_2d_example.x0.attrs, dataset_2d_example.x0.standard_name
 #     - T1 with calibration points.
 #     - T1 with calibration points and raw traces included also for the calibration points.
 #
+
+# %% [markdown]
+# Scratch
+# -------
+
+# %%
+def generate_mock_iq_data(
+    n_shots, sigma=0.3, center0=(1, 1), center1=(1, -1), prob=0.5
+):
+    """
+    Generates two clusters of I,Q points with a Gaussian distribution.
+    """
+    i_data = np.zeros(n_shots)
+    q_data = np.zeros(n_shots)
+    for i in range(n_shots):
+        c = center0 if (np.random.rand() > prob) else center1
+        i_data[i] = np.random.normal(c[0], sigma)
+        q_data[i] = np.random.normal(c[1], sigma)
+    return i_data + 1j * q_data
+
+
+def generate_exp_decay_probablity(time: np.ndarray, tau: float):
+    return np.exp(-time / tau)
+
+
+# %%
+def generate_trace_for_iq_point(
+    iq_amp: complex,
+    intermediate_freq: float = 50e6,
+    sampling_rate: float = 1e9,
+    trace_duratation: float = 1e-6,
+) -> tuple:
+    """
+    Generates mock traces that a physical instrument would digitize for the readout of
+    a transmon qubit.
+    """
+
+    trace_length = sampling_rate * trace_duratation
+    tbase = np.arange(0, trace_length, 1) / sampling_rate
+
+    return tbase, iq_amp * np.exp(2.0j * np.pi * intermediate_freq * tbase)
+
+
+def plot_centroids(ax, center_ground, center_excited):
+    ax.plot(
+        [center_ground[0]],
+        [center_ground[1]],
+        label="|0>",
+        marker="o",
+        color="C3",
+        markersize=10,
+    )
+    ax.plot(
+        [center_excited[0]],
+        [center_excited[1]],
+        label="|1>",
+        marker="^",
+        color="C4",
+        markersize=10,
+    )
+
+
+# %%
+center_ground = (-0.2, 0.65)
+center_excited = (0.7, -0, 4)
+
+shots = generate_mock_iq_data(
+    n_shots=1024, sigma=0.15, center0=center_ground, center1=center_excited, prob=0.4
+)
+
+# %%
+plt.hexbin(shots.real, shots.imag)
+plt.xlabel("I")
+plt.ylabel("Q")
+plot_centroids(plt.gca(), center_ground, center_excited)
+
+# %%
+time, trace = generate_trace_for_iq_point(shots[0])
+
+fig, ax = plt.subplots(1, 1, figsize=(30, 5))
+ax.plot(time, trace.imag, ".-")
+ax.plot(time, trace.real, ".-")
+
+# %% [raw]
+# T1 experiment averaged in hardware
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+tau = 30e-6
+center_ground = (-0.2, 0.65)
+center_excited = (0.7, -0, 4)
+num_shots = 1024
+x0s = np.linspace(0, 150e-6, 30)
+time_par = ManualParameter(name="time", label="Time", unit="s")
+q0_iq_par = ManualParameter(name="q0_iq", label="Q0 IQ amplitude", unit="V")
+
+probabilities = generate_exp_decay_probablity(time=x0s, tau=tau)
+plt.ylabel("|1> probability")
+plt.suptitle("Typical T1 experiment")
+plt.plot(x0s, probs, ".-")
+
+# %%
+y0s = np.fromiter(
+    (
+        np.average(
+            generate_mock_iq_data(
+                n_shots=num_shots,
+                sigma=0.15,
+                center0=center_ground,
+                center1=center_excited,
+                prob=prob,
+            )
+        )
+        for prob in probabilities
+    ),
+    dtype=complex,
+)
+
+dataset = dataset_2d_example = xr.Dataset(
+    data_vars={
+        "y0": ("acq_set_0", y0s, par_to_attrs(q0_iq_par)),
+    },
+    coords={
+        "x0": ("acq_set_0", x0s, par_to_attrs(time_par)),
+    },
+)
+
+
+assert dataset == dataset_round_trip(dataset)  # confirm read/write
+
+dataset
+
+# %%
+dataset_gridded = dh.to_gridded_dataset(dataset, dimension="acq_set_0")
+dataset_gridded
+
+
+# %%
+def plot_decay_no_repetition(gridded_dataset, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    gridded_dataset.y0.real.plot(ax=ax, marker=".", label="I data")
+    gridded_dataset.y0.imag.plot(ax=ax, marker=".", label="Q data")
+    # ax.set_ylabel(f"{gridded_dataset.y0.standard_name} [{gridded_dataset.y0.units}]")
+    ax.set_title(f"y0 shape = {gridded_dataset.y0.shape}")
+    ax.legend()
+    return ax.get_figure(), ax
+
+
+def plot_iq_no_repetition(gridded_dataset, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    ax.plot(
+        gridded_dataset.y0.real,
+        gridded_dataset.y0.imag,
+        ".-",
+        label="Data on IQ plane",
+        color="C2",
+    )
+    ax.set_xlabel("I")
+    ax.set_ylabel("Q")
+    plot_centroids(ax, center_ground, center_excited)
+    ax.legend()
+
+    return ax.get_figure(), ax
+
+
+# %%
+plot_decay_no_repetition(dataset_gridded)
+plot_iq_no_repetition(dataset_gridded)
+
+# %% [raw]
+# T1 experiment averaged in hardware with calibration points
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+y0s = np.fromiter(
+    (
+        np.average(
+            generate_mock_iq_data(
+                n_shots=num_shots,
+                sigma=0.15,
+                center0=center_ground,
+                center1=center_excited,
+                prob=prob,
+            )
+        )
+        for prob in probabilities
+    ),
+    dtype=complex,
+)
+
+y0s_calib = np.fromiter(
+    (
+        np.average(
+            generate_mock_iq_data(
+                n_shots=num_shots,
+                sigma=0.15,
+                center0=center_ground,
+                center1=center_excited,
+                prob=prob,
+            )
+        )
+        for prob in [0, 1]
+    ),
+    dtype=complex,
+)
+
+dataset = dataset_2d_example = xr.Dataset(
+    data_vars={
+        "y0": ("acq_set_0", y0s, par_to_attrs(q0_iq_par)),
+        "y0_calib": ("acq_set_0_calib", y0s_calib, par_to_attrs(q0_iq_par)),
+    },
+    coords={
+        "x0": ("acq_set_0", x0s, par_to_attrs(time_par)),
+        "x0_calib": (
+            "acq_set_0_calib",
+            ["0", "1"],
+            {"standard_name": "q0_state", "long_name": "Q0 State", "unit": ""},
+        ),
+    },
+)
+
+
+assert dataset == dataset_round_trip(dataset)  # confirm read/write
+
+dataset
+
+# %%
+dataset_gridded = dh.to_gridded_dataset(
+    dataset, dimension="acq_set_0", coords_names=["x0"]
+)
+dataset_gridded = dh.to_gridded_dataset(
+    dataset_gridded, dimension="acq_set_0_calib", coords_names=["x0_calib"]
+)
+dataset_gridded
+
+# %%
+fig = plt.figure(figsize=(8, 5))
+
+ax = plt.subplot2grid((1, 10), (0, 0), colspan=9, fig=fig)
+plot_decay_no_repetition(dataset_gridded, ax=ax)
+
+ax_calib = plt.subplot2grid((1, 10), (0, 9), colspan=1, fig=fig, sharey=ax)
+dataset_gridded.y0_calib.real.plot(marker="o", ax=ax_calib)
+dataset_gridded.y0_calib.imag.plot(marker="o", ax=ax_calib)
+ax_calib.yaxis.set_label_position("right")
+ax_calib.yaxis.tick_right()
+
+# plot_iq_no_repetition(dataset_gridded);
+
+# %% [raw]
+# T1 experiment storing all shots
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# %%
+y0s = np.array(
+    tuple(
+        generate_mock_iq_data(
+            n_shots=num_shots,
+            sigma=0.15,
+            center0=center_ground,
+            center1=center_excited,
+            prob=prob,
+        )
+        for prob in probabilities
+    )
+).T
+
+dataset = dataset_2d_example = xr.Dataset(
+    data_vars={
+        "y0": ("acq_set_0", y0s.mean(axis=0), par_to_attrs(q0_iq_par)),
+        "y0_shots": (("repetition", "acq_set_0"), y0s, par_to_attrs(q0_iq_par)),
+    },
+    coords={
+        "x0": ("acq_set_0", x0s, par_to_attrs(time_par)),
+    },
+)
+
+
+assert dataset == dataset_round_trip(dataset)  # confirm read/write
+
+dataset
+
+# %%
+gridded_dataset = dh.to_gridded_dataset(dataset, dimension="acq_set_0")
+gridded_dataset
+
+# %% [markdown]
+# In this dataset we have both the averaged values and all the shots. The averaged values can be plotted in the same way:
+
+# %%
+plot_iq_decay_no_repetition(gridded_dataset)
+
+# %% [markdown]
+# We can inspect how the individual shots are distributed on the IQ plane for some particular `Time` value:
+
+# %%
+for t_example in [x0s[len(x0s) // 5], x0s[-5]]:
+    shots_example = (
+        gridded_dataset.y0_shots.real.sel(x0=t_example),
+        gridded_dataset.y0_shots.imag.sel(x0=t_example),
+    )
+    plt.hexbin(*shots_example)
+    plt.xlabel("I")
+    plt.ylabel("Q")
+    plot_centroids(plt.gca(), center_ground, center_excited)
+    plt.suptitle(f"Shots fot t = {t_example:.5f} s")
+    plt.show()
+
+
+# %% [markdown]
+# We can colapse (average) along the `repetion` dimensions:
+
+# %%
+def plot_iq_decay_repetition(gridded_dataset):
+    gridded_dataset.y0_shots.real.mean(dim="repetition").plot(
+        marker=".", label="I data"
+    )
+    gridded_dataset.y0_shots.imag.mean(dim="repetition").plot(
+        marker=".", label="Q data"
+    )
+    plt.ylabel(f"{gridded_dataset.y0.standard_name} [{gridded_dataset.y0.units}]")
+    plt.suptitle("y0 shape = {}")
+    plt.legend()
+
+    # visualize data on the IQ plane
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(
+        gridded_dataset.y0_shots.real.mean(
+            dim="repetition"
+        ),  # "collapses" outer dimension
+        gridded_dataset.y0_shots.imag.mean(
+            dim="repetition"
+        ),  # "collapses" outer dimension
+        ".-",
+        label="Data on IQ plane",
+        color="C2",
+    )
+    ax.set_xlabel("I")
+    ax.set_ylabel("Q")
+    plot_centroids(ax, center_ground, center_excited)
+    ax.legend()
+
+
+# %%
+plot_iq_decay_repetition(gridded_dataset)
