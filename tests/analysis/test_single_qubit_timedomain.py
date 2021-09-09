3cfa5c9c9e28a2f18f1ad92874d33153fe4c9ef8
adriaanrol <adriaan.rol@gmail.com>
2021-09-09 19:49:16 +0200

    moved T1 analysis over to new single-qubit timedomain file

--- tests/analysis/test_single_qubit_timedomain.py
+++ tests/analysis/test_single_qubit_timedomain.py
@@ -1,6 +1,17 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=redefined-outer-name  # in order to keep the fixture in the same file
+import pytest
+from pytest import approx
+from uncertainties.core import Variable
+from quantify_core.data.handling import set_datadir
+
+
 import numpy as np
 from quantify_core.analysis.single_qubit_timedomain import (
     rotate_to_calibrated_axis,
+    T1Analysis,
 )
 
 
@@ -14,4 +25,49 @@ def test_rotate_to_calibrated_axis():
         data, ref_val_0=ref_val_0, ref_val_1=ref_val_1
     )
 
-    assert corrected_data == np.array([0.0, 1.0])
+    np.testing.assert_array_equal(corrected_data, np.array([0.0, 1.0]))
+
+
+tuid_list = ["20210322-205253-758-6689"]
+t1_list = [1.07e-5]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def analyses(tmp_test_data_dir):
+    """
+    Used to run the analysis a single time and run unit tests against the created
+    analysis object.
+    """
+    set_datadir(tmp_test_data_dir)
+    analyses = [T1Analysis(tuid=tuid).run() for tuid in tuid_list]
+
+    return analyses
+
+
+def test_figures_generated(analyses):
+    """
+    Test that the right figures get created.
+    """
+    for analysis in analyses:
+        assert set(analysis.figs_mpl.keys()) == {
+            "T1_decay",
+        }
+
+
+def test_quantities_of_interest(analyses):
+    """
+    Test that the fit returns the correct values
+    """
+    for analysis, t_1 in zip(analyses, t1_list):
+        assert set(analysis.quantities_of_interest.keys()) == {
+            "T1",
+            "fit_msg",
+            "fit_result",
+            "fit_success",
+        }
+
+        assert isinstance(analysis.quantities_of_interest["T1"], Variable)
+        # Tests that the fitted values are correct (to within 5 standard deviations)
+        assert analysis.quantities_of_interest["T1"].nominal_value == approx(
+            t_1, abs=5 * analysis.quantities_of_interest["T1"].std_dev
+        )
