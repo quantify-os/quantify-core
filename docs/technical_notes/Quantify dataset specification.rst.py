2372bcc51398714ace0b49417b80b3f5a846d643
caenrigen <vnegirneac@qblox.com>
2021-09-13 18:54:49 +0200

    include jupytext header

--- docs/technical_notes/Quantify dataset specification.rst.py
+++ docs/technical_notes/Quantify dataset specification.rst.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %% [raw]
 # Quantify dataset specification
 # ==============================
