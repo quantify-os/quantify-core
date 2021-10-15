edf0a6be04d6f5321cdf100df51de184b8d9ef99
caenrigen <vnegirneac@qblox.com>
2021-10-15 10:41:25 +0200

    Revert logging for filelock (was fixed upstream)

--- quantify_core/measurement/control.py
+++ quantify_core/measurement/control.py
@@ -3,7 +3,6 @@
 """Module containing the MeasurementControl."""
 from __future__ import annotations
 
-import logging
 import time
 import json
 import types
@@ -39,10 +38,6 @@ from quantify_core.measurement.types import Settable, Gettable, is_batched
 
 # Intended for plotting monitors that run in separate processes
 _DATASET_LOCKS_DIR = tempfile.gettempdir()
-
-# Silence irrelevant logs from filelock that would fail the docs CI
-# https://github.com/tox-dev/py-filelock/issues/104#issuecomment-940020884
-logging.getLogger("filelock").setLevel(logging.WARNING)
 
 
 class MeasurementControl(Instrument):  # pylint: disable=too-many-instance-attributes
