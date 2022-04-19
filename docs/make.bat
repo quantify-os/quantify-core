a248853f4983d590977a5d6f3c3c37ed53f26bb6
Edgar Reehuis <10068237-ereehuis@users.noreply.gitlab.com>
2022-04-19 12:26:23 +0200

    Try and reinstate fail on warnings for Sphinx

--- docs/make.bat
+++ docs/make.bat
@@ -14,7 +14,7 @@ set SPHINXPROJ=quantify_core
 REM -vv can be appended below to activate sphinx verbose mode
 REM For a reference of the different sphinxopts flags,
 REM see https://www.sphinx-doc.org/en/master/man/sphinx-build.html
-set SPHINXOPTS=--keep-going -n -w build_errors.log
+set SPHINXOPTS=-W --keep-going -n -w build_errors.log
 
 if "%1" == "" goto help
 
