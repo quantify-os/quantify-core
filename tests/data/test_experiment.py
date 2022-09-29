0918737abcb1a7ad22a4978dadce6ffe26d9f788
adamorangeqs <adam@orangeqs.com>
2022-09-29 16:16:35 +0200

    save name of experiment

--- tests/data/test_experiment.py
+++ tests/data/test_experiment.py
@@ -2,7 +2,8 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
-
+import numpy as np
+import xarray as xr
 import pytest
 
 from quantify_core.data import handling as dh
@@ -52,3 +53,35 @@ def test_quantify_experiment_load_and_save_metadata(tmp_test_data_dir):
     dictionary = {"key": "entry to be saved"}
     experiment.save_metadata(dictionary)
     assert experiment.load_metadata() == dictionary
+
+
+def test_quantify_experiment_save_and_load_dataset(tmp_test_data_dir):
+    complex_float = 1.0 + 6.0j
+    complex_int = 1 + 3j
+    dataset = mk_dataset_complex_array(
+        complex_float=complex_float, complex_int=complex_int
+    )
+    dataset.attrs["name"] = "test_dataset"
+    tuid = TUID_1D_1PLOT
+
+    dh.set_datadir(tmp_test_data_dir)
+
+    experiment = QuantifyExperiment(tuid)
+
+    experiment.write_dataset(dataset)
+
+    load_dataset = experiment.load_dataset()
+    assert load_dataset.y0.values[0] == complex_int
+    assert load_dataset.y1.values[0] == complex_float
+    assert load_dataset.name == "test_dataset"
+
+
+def mk_dataset_complex_array(complex_float=1.0 + 5.0j, complex_int=1 + 4j):
+    dataset = xr.Dataset(
+        data_vars={
+            "y0": ("dim_0", np.array([complex_int] * 5)),
+            "y1": ("dim_0", np.array([complex_float] * 5)),
+        },
+        coords={"x0": np.linspace(0, 5, 5)},
+    )
+    return dataset
