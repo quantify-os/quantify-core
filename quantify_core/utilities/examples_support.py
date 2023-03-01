7933f9d09442f41fb25e782e1b1e2107e6db02a3
Robert Sokolewicz <rsokolewicz@gmail.com>
2023-03-01 18:18:26 +0100

    type hinting

--- quantify_core/utilities/examples_support.py
+++ quantify_core/utilities/examples_support.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 from pathlib import Path
 from time import sleep
-from typing import TYPE_CHECKING, Any, Callable, Dict, Tuple, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
 import xarray as xr
@@ -203,11 +203,7 @@ def mk_trace_time(sampling_rate: float = 1e9, duration: float = 0.3e-6) -> NDArr
 
 def mk_trace_for_iq_shot(
     iq_point: complex,
-<<<<<<< HEAD
-    time_values: None | NDArray = None,
-=======
-    time_values: NDArray = mk_trace_time(),
->>>>>>> origin
+    time_values: Optional[NDArray] = None,
     intermediate_freq: float = 50e6,
 ) -> NDArray:
     """
