0cf4778c188909406cf29d5092bbd8bb5c270b0c
avarshiny <amirthavarshiny@orangeqs.com>
2025-07-14 17:05:38 +0200

    adding deprecation warning to package init

--- quantify_core/__init__.py
+++ quantify_core/__init__.py
@@ -1,5 +1,17 @@
 # pylint: disable=django-not-configured
+import warnings
+
+warnings.warn(
+    "This package has reached its end of life. "
+    "It is no longer maintained and will not receive any further updates or support. "
+    "For further developments, please refer to the new Quantify repository: https://gitlab.com/quantify-os/quantify",
+    DeprecationWarning,
+    stacklevel=2,
+)
 from . import utilities
 from ._version import __version__
 
 __all__ = ["utilities"]
+
+
+
