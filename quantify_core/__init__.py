bff283f497e0d0344f1b73c934325da53ba305c6
avarshiny <amirthavarshiny@orangeqs.com>
2025-07-14 17:11:13 +0200

    pre-commit

--- quantify_core/__init__.py
+++ quantify_core/__init__.py
@@ -1,6 +1,9 @@
 # pylint: disable=django-not-configured
 import warnings
 
+from . import utilities
+from ._version import __version__
+
 warnings.warn(
     "This package has reached its end of life. "
     "It is no longer maintained and will not receive any further updates or support. "
@@ -8,10 +11,4 @@ warnings.warn(
     DeprecationWarning,
     stacklevel=2,
 )
-from . import utilities
-from ._version import __version__
-
 __all__ = ["utilities"]
-
-
-
