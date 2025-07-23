1030981e7e45a8552a8c24bde264d396ef513b96
Robert Sokolewicz <rsokolewicz@gmail.com>
2025-07-23 15:35:05 +0200

    chore: remove warning

--- quantify_core/__init__.py
+++ quantify_core/__init__.py
@@ -1,14 +1,5 @@
 # pylint: disable=django-not-configured
-import warnings
-
 from . import utilities
 from ._version import __version__
 
-warnings.warn(
-    "This package has reached its end of life. "
-    "It is no longer maintained and will not receive any further updates or support. "
-    "For further developments, please refer to the new Quantify repository: https://gitlab.com/quantify-os/quantify",
-    DeprecationWarning,
-    stacklevel=2,
-)
 __all__ = ["utilities"]
