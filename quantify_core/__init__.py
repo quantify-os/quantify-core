69ded6be87a23786e062bcd96d2ea33f9f6c44f8
Amirtha Varshiny Arumugam <amirthavarshiny@orangeqs.com>
2025-08-18 10:28:28 +0200

    adding back warnings on package import

--- quantify_core/__init__.py
+++ quantify_core/__init__.py
@@ -2,4 +2,15 @@
 from . import utilities
 from ._version import __version__
 
+import warnings
+
+warnings.warn(
+    "This package has reached its end of life. "
+    "It is no longer maintained and will not receive any further updates or support. "
+    "For further developments, please refer to the new Quantify repository: https://gitlab.com/quantify-os/quantify."
+    "All existing functionalities can be accessed via the new Quantify repository.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 __all__ = ["utilities"]
